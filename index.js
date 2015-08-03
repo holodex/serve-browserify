991e25f58843de761746cc1038f51d55be73ba83
Michael Williams <dinosaur@riseup.net>
2015-08-03 18:18:53 +1200

    in the beginning

---
+++ index.js
@@ -0,0 +1,27 @@
+var httpError = require('http-errors')
+var browserify = require('browserify-incremental')
+
+module.exports = serveBrowserify
+
+function serveBrowserify (files, opts) {
+  var b = browserify(files, opts)
+
+  return function serve (req, res, next) {
+
+    function onError (err) {
+      next(httpError(500, err.message, err))
+    }
+
+    if (req.method.toLowerCase() === 'get') {
+      res.setHeader('Content-Type', 'application/javascript')
+
+      b
+      .on('error', onError)
+      .bundle()
+      .on('error', onError)
+      .pipe(res)
+    } else {
+      next(httpError(404))
+    }
+  }
+}
---
+++ test/index.js
@@ -0,0 +1,34 @@
+var test = require('tape')
+var fs = require('fs')
+var mocks = require('node-mocks-http')
+
+var sendBrowserify = require('../')
+
+test('should send bundles', function (t) {
+  var req = mocks.createRequest({
+    method: 'GET'
+  })
+  var res = mocks.createResponse({
+    eventEmitter: require('events').EventEmitter
+  })
+  var send = sendBrowserify({
+    entries: [__dirname + '/fixtures/prebundle.js'],
+    fullPaths: false
+  })
+
+  send(req, res, function (err) {
+    t.error(err, 'should not error')
+    t.end()
+  })
+
+  t.equal(res.statusCode, 200, 'status is correct')
+  t.equal(res.get('Content-Type'), 'application/javascript', 'content type is correct')
+  res.on('end', function () {
+    t.equal(
+      res._getData(),
+      fs.readFileSync(__dirname + '/fixtures/postbundle.js').toString(),
+      'bundle is correct'
+    )
+    t.end()
+  })
+})
